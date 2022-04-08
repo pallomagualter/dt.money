0cfb73e3f8c955d1e7a18c25f9ec4b7d7b92600a
pallomagualter <pallomagualter@gmail.com>
2022-04-08 12:48:33 -0300

    colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import './App.css';
 import { GlobalStyle } from './styles/global';
 
 export function App() {
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -2,7 +2,16 @@ import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
   :root {
-    --background: #f0f2f5;
+    --red: #E52E4F;
+    --blue: #5429CC;
+
+    --blue-light: #6933FF;
+
+    --text-title: #363F5F;
+    --text-body: #969CB3;
+
+    --background: #F0F2F5;
+    --shape: #FFFFFF;
   }
 
   * {
@@ -13,16 +22,25 @@ export const GlobalStyle = createGlobalStyle`
 
   html {
     @media (max-width: 1080px) {
-      font-size: 93.75%;
+      font-size: 93.75%; //15px
     }
 
     @media (max-width: 720px) {
-      font-size: 87.5%;
+      font-size: 87.5%; //14px
     }
   }
 
   body {
     background: var(--background);
     -webkit-font-smoothing: antialiased;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  [disabled] {
+    opacity: 0.6;
+    cursor: not-allowed;
   }
 `
