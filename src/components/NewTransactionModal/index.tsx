c8f183f9b230131f0f0799917fbbae2780cf5f72
pallomagualter <pallomagualter@gmail.com>
2022-04-19 18:07:11 -0300

    layout modal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,8 @@
 import Modal from 'react-modal';
 import { Container } from './styles';
 
+import closeImg from '../../assets/close.svg'
+
 interface NewTransactionModalProps {
   isOpen: boolean;
   onRequestClose: () => void;
@@ -11,7 +13,17 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
     <Modal
       isOpen={isOpen}
       onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
     >
+      <button
+        type="button"
+        onClick={onRequestClose}
+        className="react-modal-close"
+      >
+        <img src={closeImg} alt="Fechar modal" />
+      </button>
+
       <Container>
         <h2>Cadastrar transação</h2>
 
--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -16,6 +16,36 @@ export const Container = styled.form`
 
     border: 1px solid #D7D7D7;
     background: #E7E9EE;
+
+    font-weight: 400;
+    font-size: 1rem;
+
+    &::placeholder {
+      color: var(--text-body);
+    }
+
+    & + input {
+      margin-top: 1rem;
+    }
   }
-  
+
+  button[type="submit"] {
+    width: 100%;
+    padding: 0 1.5rem;
+    height: 4rem;
+    background: var(--green);
+    color: var(--shape);
+    border: 0;
+    border-radius: 0.25rem;
+    font-size: 1rem;
+    margin-top: 1.5rem;
+    font-weight: 600;
+
+    transition: filter 0.2s;
+
+    &:hover {
+      filter: brightness(0.9);
+    }
+  }
+
 `;
