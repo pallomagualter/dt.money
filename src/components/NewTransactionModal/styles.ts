c892ba50aa6b44979772ad5d507328517e4a4098
pallomagualter <pallomagualter@gmail.com>
2022-04-19 17:44:50 -0300

    styles modal

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,4 +1,21 @@
 import styled from "styled-components";
 
 
-export const Container = styled.form``;
+export const Container = styled.form`
+  h2 {
+    color: var(--text-title);
+    font-size: 1.5rem;
+    margin-bottom: 2rem;
+  }
+
+  input {
+    width: 100%;
+    padding: 0 1.5rem;
+    height: 4rem;
+    border-radius: 0.25rem;
+
+    border: 1px solid #D7D7D7;
+    background: #E7E9EE;
+  }
+  
+`;
